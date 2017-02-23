eb87dd1e88e0355fb472da12406a72b905b8496e
coreytegeler <coreytegeler@gmail.com>
2017-02-22 20:50:09 -0500

    first round of feedback

--- coffee/scripts.coffee
+++ coffee/scripts.coffee
@@ -5,3 +5,13 @@ $ ->
     navigationTooltips: ['Intro', 'Overview', 'Quiz', 'Hygiene', 'Recommend', 'Risk', 'Patrons', 'Luca', 'Jo', 'Sara', 'Xav'],
     slidesNavigation: true,
     scrollingSpeed: 1000
+
+  $('.bhoechie-tab-menu .list-group a').click (e) ->
+    e.preventDefault()
+    $(this).siblings('a.active').removeClass('active')
+    $(this).addClass('active')
+    index = $(this).index()
+    $container = $(this).parents('.bhoechie-tab-container')
+    $content = $container.find('.bhoechie-tab-content')
+    $content.removeClass('active');
+    $content.eq(index).addClass('active');
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 (function() {
   $(function() {
-    return $('#fullpage').fullpage({
+    $('#fullpage').fullpage({
       anchors: ['intro', 'overview', 'quiz', 'hygiene', 'recommend', 'risk', 'patrons', 'luca', 'jo', 'sara', 'xav'],
       navigation: true,
       navigationTooltips: ['Intro', 'Overview', 'Quiz', 'Hygiene', 'Recommend', 'Risk', 'Patrons', 'Luca', 'Jo', 'Sara', 'Xav'],
       slidesNavigation: true,
       scrollingSpeed: 1000
+    });
+    return $('.bhoechie-tab-menu .list-group a').click(function(e) {
+      var $container, $content, index;
+      e.preventDefault();
+      $(this).siblings('a.active').removeClass('active');
+      $(this).addClass('active');
+      index = $(this).index();
+      $container = $(this).parents('.bhoechie-tab-container');
+      $content = $container.find('.bhoechie-tab-content');
+      $content.removeClass('active');
+      return $content.eq(index).addClass('active');
     });
   });
 
